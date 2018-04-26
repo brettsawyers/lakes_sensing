c08af0972d536c694576fd121b1613e9dbe6dd7c
brettsawyers <brett.sawyers@uqconnect.edu.au>
2018-04-26 12:10:42 +0000

    Now sending what loos like a correct message to influxdb, but it keeps saying there is an invalid number in the string

--- serverapps/parser.js
+++ serverapps/parser.js
@@ -11,30 +11,35 @@ http.createServer(function(request,response) {
 		if(recvd.hasOwnProperty('data')) {
 			//check for existence of the data key
 			let data = JSON.parse(new Buffer(recvd['data'], 'hex'));
-
-            data['temperature'] = data['a'];
+            let influxstr = "airsensors,device=" + recvd['devaddr'] +
+                " Temperature=" + data['a'] +",Humidity=" + data['b'] +
+                ",CO2=" + data['c'] + ",NO2=" + data['d'] + ",Ozone=" +
+                data['e'] + ",CO=" + data['f'] + ",Battery=" + data['g'];
+/*
+            data['temperature_air'] = data['a'];
             delete data['a'];
-            data['humidity'] = data['b'];
+            data['humidity_air'] = data['b'];
             delete data['b'];
-            data['CO2'] = data['c'];
+            data['CO2_air'] = data['c'];
             delete data['c'];
-            data['NO2'] = data['d'];
+            data['NO2_air'] = data['d'];
             delete data['d'];
-            data['Ozone'] = data['e'];
+            data['Ozone_air'] = data['e'];
             delete data['e'];
-            data['CO'] = data['f'];
+            data['CO_air'] = data['f'];
             delete data['f'];
-            data['Battery'] = data['g'];
+            data['Battery_air'] = data['g'];
             delete data['g'];
-			//data should now be a JSON object, format correctly and then POST to influx
-			console.log('data temp = %s', data['temperature']);
+*/
+			//data should now be a string object, formatted correctly and ready to POST to influx
+			console.log('%s',influxstr);
 			requestfunc({
-				url: "http://localhost:9100",
+				url: "http://localhost:8086/write?db=testdb",
 				method: "POST",
 				json: true,
-				body: data,
+				body: influxstr,
 			}, function (error, res, body){
-
+                console.log(body);
 			});
 		}
 		console.log('recieved: %s', recvd['app']);
