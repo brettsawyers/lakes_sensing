f9f14edd8c33495115c50f917013c449a4802a87
brettsawyers <brett.sawyers@uqconnect.edu.au>
2018-04-26 10:31:01 +0000

    Can now decode and remake a new json obkect that is ready to be posted to the influxdb databse

--- serverapps/parser.js
+++ serverapps/parser.js
@@ -11,8 +11,23 @@ http.createServer(function(request,response) {
 		if(recvd.hasOwnProperty('data')) {
 			//check for existence of the data key
 			let data = JSON.parse(new Buffer(recvd['data'], 'hex'));
+
+            data['temperature'] = data['a'];
+            delete data['a'];
+            data['humidity'] = data['b'];
+            delete data['b'];
+            data['CO2'] = data['c'];
+            delete data['c'];
+            data['NO2'] = data['d'];
+            delete data['d'];
+            data['Ozone'] = data['e'];
+            delete data['e'];
+            data['CO'] = data['f'];
+            delete data['f'];
+            data['Battery'] = data['g'];
+            delete data['g'];
 			//data should now be a JSON object, format correctly and then POST to influx
-			console.log('data a = %s', data['a']);
+			console.log('data temp = %s', data['temperature']);
 			requestfunc({
 				url: "http://localhost:9100",
 				method: "POST",
@@ -20,9 +35,9 @@ http.createServer(function(request,response) {
 				body: data,
 			}, function (error, res, body){
 
-			});	
+			});
 		}
 		console.log('recieved: %s', recvd['app']);
 	});
-	
+
 }).listen(8080);
