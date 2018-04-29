461eafb47fd52fe54ea955037444da9a5428e990
brettsawyers <brett.sawyers@uqconnect.edu.au>
2018-04-29 22:19:08 +0000

    Added in some nodjs to pretend to be a node sendijng out packets so grafana will be a bit more interesting

---
+++ serverapps/nodeemulator.js
@@ -0,0 +1,11 @@
+var sys = require('util');
+var exec = require('child_process').exec;
+var datastr = "\"data\":\"7b2261223a3130302c2262223a3230302c2263223a31322c2264223a302e3231322c2265223a3135362c2266223a343934322c2267223a32377d\"";
+var devaddrstr = "\"devaddr\":";
+var appstr = "curl -X POST -d '{\"app\":\"sensortest\"";
+function puts(error, stdout, stderr) {sys.puts(stdout)}
+var command = appstr + "," + datastr + "," + devaddrstr + "\"AB0123\"}'" + " http://localhost:8080";
+exec(command,function(err, stdout, stderr){
+    console.log(stdout);
+    console.log(command);
+});
